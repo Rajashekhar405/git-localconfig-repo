3358977326fb37ba69dd2e2b5df46d03bb82ccad
rajashekhar <rajashekhar405@gmail.com>
2020-05-10 11:01:00 +0530

    first comit

---
+++ src/main/java/com/example/boot_demo_cloudconfig/BootDemoCloudconfigApplication.java
@@ -0,0 +1,13 @@
+package com.example.boot_demo_cloudconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootDemoCloudconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootDemoCloudconfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/boot_demo_cloudconfig/BootDemoCloudconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boot_demo_cloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootDemoCloudconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
